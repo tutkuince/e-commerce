fa85f3c7ff757d97b2a80f1635bb6249bf7b9118
TUTKU\tutku <tutku.ince@outlook.com>
2024-06-27 22:16:09 +0300

    Creating the AWS ECR repository with AWS CDK

--- ECommerceECS_CDK/src/main/java/com/myorg/EcrStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/EcrStack.java
@@ -1,12 +1,28 @@
 package com.myorg;
 
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecr.RepositoryProps;
+import software.amazon.awscdk.services.ecr.TagMutability;
 import software.constructs.Construct;
 
 public class EcrStack extends Stack {
+    private final Repository productServiceRepository;
 
     public EcrStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        this.productServiceRepository = new Repository(this, "product-service", RepositoryProps.builder()
+                .repositoryName("product-service")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .imageTagMutability(TagMutability.IMMUTABLE)
+                .autoDeleteImages(true)
+                .build());
+    }
+
+    public Repository getProductServiceRepository() {
+        return productServiceRepository;
     }
 }
