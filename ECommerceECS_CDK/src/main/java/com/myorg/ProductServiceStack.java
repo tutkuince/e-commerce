1a3fc43666e09bd6ec945cc6b86e2640120ec9ce
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-01 14:12:35 +0300

    Creating the new stack for the ProductService

---
+++ ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
@@ -0,0 +1,25 @@
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ecr.Repository;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancer;
+import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
+import software.constructs.Construct;
+
+public class ProductServiceStack extends Stack {
+
+    public ProductServiceStack(final Construct scope, final String id, StackProps stackProps, ProductServiceProps productServiceProps) {
+        super(scope, id, stackProps);
+    }
+}
+
+record ProductServiceProps(
+        Vpc vpc,
+        Cluster cluster,
+        NetworkLoadBalancer networkLoadBalancer,
+        ApplicationLoadBalancer applicationLoadBalancer,
+        Repository repository
+) {}
