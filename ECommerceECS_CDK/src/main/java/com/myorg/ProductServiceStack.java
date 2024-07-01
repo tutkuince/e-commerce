e0a5ba555043f7bb275cb28e1623b68d69e852ca
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-01 14:29:37 +0300

    Creating the service log driver

--- ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
@@ -1,14 +1,16 @@
 package com.myorg;
 
+import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecr.Repository;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.FargateTaskDefinition;
-import software.amazon.awscdk.services.ecs.FargateTaskDefinitionProps;
+import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancer;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.LogGroupProps;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.constructs.Construct;
 
 public class ProductServiceStack extends Stack {
@@ -20,6 +22,15 @@ public class ProductServiceStack extends Stack {
                 .family("product-service")
                 .cpu(512)
                 .memoryLimitMiB(1024)
+                .build());
+
+        AwsLogDriver logDriver = new AwsLogDriver(AwsLogDriverProps.builder()
+                .logGroup(new LogGroup(this, "LogGroup", LogGroupProps.builder()
+                        .logGroupName("ProductService")
+                        .removalPolicy(RemovalPolicy.DESTROY)
+                        .retention(RetentionDays.ONE_MONTH)
+                        .build()))
+                .streamPrefix("ProductService")
                 .build());
     }
 }
