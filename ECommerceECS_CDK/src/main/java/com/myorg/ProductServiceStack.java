41413a1296e0c5a218e171ec4d8c32533dc5b74e
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-01 14:22:19 +0300

    Creating the ECS task definition

--- ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
@@ -5,6 +5,8 @@ import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.ecr.Repository;
 import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.FargateTaskDefinition;
+import software.amazon.awscdk.services.ecs.FargateTaskDefinitionProps;
 import software.amazon.awscdk.services.elasticloadbalancingv2.ApplicationLoadBalancer;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
 import software.constructs.Construct;
@@ -13,6 +15,12 @@ public class ProductServiceStack extends Stack {
 
     public ProductServiceStack(final Construct scope, final String id, StackProps stackProps, ProductServiceProps productServiceProps) {
         super(scope, id, stackProps);
+
+        FargateTaskDefinition fargateTaskDefinition = new FargateTaskDefinition(this, "TaskDefinition", FargateTaskDefinitionProps.builder()
+                .family("product-service")
+                .cpu(512)
+                .memoryLimitMiB(1024)
+                .build());
     }
 }
 
