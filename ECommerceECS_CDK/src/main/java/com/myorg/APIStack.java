542352eb56616f31f4be0e8100547d5a1666612f
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-04 20:25:44 +0300

    Creating the API Gateway resource

--- ECommerceECS_CDK/src/main/java/com/myorg/APIStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/APIStack.java
@@ -2,6 +2,8 @@ package com.myorg;
 
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.apigateway.RestApi;
+import software.amazon.awscdk.services.apigateway.RestApiProps;
 import software.amazon.awscdk.services.apigateway.VpcLink;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
 import software.constructs.Construct;
@@ -14,6 +16,10 @@ public class APIStack extends Stack {
             StackProps stackProps,
             APIStackProps apiStackProps) {
         super(scope, id, stackProps);
+
+        RestApi restApi = new RestApi(this, "RestAPI", RestApiProps.builder()
+                .restApiName("ECommerceAPI")
+                .build());
 
         
     }
