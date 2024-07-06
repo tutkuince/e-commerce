27d4547ddd2b7151a313c21c74b56c1cc890f668
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-06 18:31:24 +0300

    Creating the REST operation to update a product by its id

--- ECommerceECS_CDK/src/main/java/com/myorg/APIStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/APIStack.java
@@ -6,6 +6,9 @@ import software.amazon.awscdk.services.apigateway.*;
 import software.amazon.awscdk.services.elasticloadbalancingv2.NetworkLoadBalancer;
 import software.constructs.Construct;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class APIStack extends Stack {
 
     public APIStack(
@@ -54,8 +57,28 @@ public class APIStack extends Stack {
                         .build()
         ));
 
-
         // GET /products/{id}
+        Map<String, String> productIdIntegrationParameters = new HashMap<>();
+        productIdIntegrationParameters.put("integration.request.path.id", "method.request.path.id");
+
+        Map<String, Boolean> productIdMethodParameters = new HashMap<>();
+        productIdMethodParameters.put("method.request.path.id", true);
+
+        Resource productIdResource = productResource.addResource("{id}");
+        productIdResource.addMethod("PUT", new Integration(
+                IntegrationProps.builder()
+                        .type(IntegrationType.HTTP_PROXY)
+                        .integrationHttpMethod("PUT")
+                        .uri("http://" + apiStackProps.networkLoadBalancer().getLoadBalancerDnsName() +
+                                ":8080/api/products/{id}")
+                        .options(IntegrationOptions.builder()
+                                .vpcLink(apiStackProps.vpcLink())
+                                .connectionType(ConnectionType.VPC_LINK)
+                                .requestParameters(productIdIntegrationParameters)
+                                .build())
+                        .build()), MethodOptions.builder()
+                .requestParameters(productIdMethodParameters)
+                .build());
 
         // PUT /products/{id}
 
