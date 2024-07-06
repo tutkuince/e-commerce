5d2983840e2ed8f82b8b907bc2f100fea0454afd
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-06 21:44:08 +0300

    update Product recoord to class

--- product-service/src/main/java/com/incetutku/productservice/products/model/Product.java
+++ product-service/src/main/java/com/incetutku/productservice/products/model/Product.java
@@ -4,12 +4,51 @@ import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
 
 @DynamoDbBean
-public record Product(
-        @DynamoDbPartitionKey
-        String id,
-        String productName,
-        String code,
-        float price,
-        String model
-) {
+public class Product {
+    private String id;
+    private String productName;
+    private String code;
+    private float price;
+    private String model;
+
+    @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
 }
