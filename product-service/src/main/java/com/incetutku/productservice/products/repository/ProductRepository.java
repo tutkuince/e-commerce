6020f1e14a0b094d48f1f1256a968b869f64e23e
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-06 20:42:38 +0300

    creating the product repository

---
+++ product-service/src/main/java/com/incetutku/productservice/products/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.incetutku.productservice.products.repository;
+
+import com.incetutku.productservice.products.model.Product;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbAsyncTable;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+
+@Repository
+public class ProductRepository {
+    private final DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient;
+    private final DynamoDbAsyncTable<Product> productTable;
+
+    public ProductRepository(DynamoDbEnhancedAsyncClient dynamoDbEnhancedAsyncClient, @Value("${aws.product.db.name}") String productDBName) {
+        this.dynamoDbEnhancedAsyncClient = dynamoDbEnhancedAsyncClient;
+        this.productTable = dynamoDbEnhancedAsyncClient.table(productDBName, TableSchema.fromBean(Product.class));
+    }
+}
