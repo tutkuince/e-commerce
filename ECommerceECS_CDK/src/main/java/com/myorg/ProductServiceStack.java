71b1ffdcebffe810cc9c57783296adf5b7247c4b
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-08 22:21:32 +0300

    version upgrade

--- ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/ProductServiceStack.java
@@ -65,7 +65,7 @@ public class ProductServiceStack extends Stack {
         envVariables.put("AWS_REGION", this.getRegion());
 
         fargateTaskDefinition.addContainer("ProductServiceContainer", ContainerDefinitionOptions.builder()
-                .image(ContainerImage.fromEcrRepository(productServiceProps.repository(), "1.1.1"))
+                .image(ContainerImage.fromEcrRepository(productServiceProps.repository(), "1.1.2"))
                 .containerName("productService")
                 .logging(logDriver)
                 .portMappings(Collections.singletonList(PortMapping.builder()
