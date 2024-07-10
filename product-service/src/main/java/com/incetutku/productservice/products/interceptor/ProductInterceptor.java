7c2ef09fc27d4b8d9d85ed20f5f9deff46676ddf
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-10 22:49:29 +0300

    Capturing the unique request id from AWS API Gateway with an interceptor

---
+++ product-service/src/main/java/com/incetutku/productservice/products/interceptor/ProductInterceptor.java
@@ -0,0 +1,22 @@
+package com.incetutku.productservice.products.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class ProductInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        ThreadContext.put("requestId", request.getHeader("requestId"));
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        ThreadContext.clearAll();
+    }
+}
