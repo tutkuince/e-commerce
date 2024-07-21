4dc674218843540748be0caf015b6d61fd6741eb
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-21 16:23:23 +0300

    Adding the product code as a non-required parameter in the API Gateway resource

--- ECommerceECS_CDK/src/main/java/com/myorg/APIStack.java
+++ ECommerceECS_CDK/src/main/java/com/myorg/APIStack.java
@@ -61,10 +61,12 @@ public class APIStack extends Stack {
 
         Map<String, Boolean> productMethodParameters = new HashMap<>();
         productMethodParameters.put("method.request.header.requestId", false);
+        productMethodParameters.put("method.request.querystring.code", false);
 
         // /products
         Resource productResource = restApi.getRoot().addResource("products");
         // GET: /products
+        // GET: /products?code=CODE1
         productResource.addMethod("GET", new Integration(
                 IntegrationProps.builder()
                         .type(IntegrationType.HTTP_PROXY)
